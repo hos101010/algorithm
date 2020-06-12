cab7fb7d942cd7686629b67746267cada60356ce
hos101010 <hos101010@naver.com>
2020-06-13 05:16:07 +0900

    doc: revise baekjoon/15665

--- baekjoon/15665_N과 M(11).cpp
+++ baekjoon/15665_N과 M(11).cpp
@@ -1,41 +1,40 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int n, m;
-int arr[8];
-int new_arr[8];
+vector<int> num;
+vector<int> res;
 
 void dfs(int cnt) {
-	bool used[10001] = { false, };
-
+	vector<int> history;
 	if (cnt == m) {
 		for (int i = 0; i < m; i++) {
-			cout << new_arr[i] << " ";
+			cout << res[i] << " ";
 		}
 		cout << "\n";
 		return;
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (!used[arr[i]]) {
-			used[arr[i]] = true;
-			new_arr[cnt++] = arr[i];
-			dfs(cnt);
-			cnt--;
-		}
+		if (count(history.begin(), history.end(), num[i]) > 0) continue;
+		res[cnt] = num[i];
+		history.push_back(num[i]);
+		dfs(cnt + 1);
 	}
 }
 
 int main() {
 	cin >> n >> m;
+	int input;
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> input;
+		num.push_back(input);
+		if (i < m) res.push_back(0);
 	}
+	sort(num.begin(), num.end());
 
-	sort(arr, arr + n);
 	dfs(0);
-
 	return 0;
 }
