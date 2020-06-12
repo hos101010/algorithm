7e45a7caab523bab998e3dd4f94fec8031c9dea5
hos101010 <hos101010@naver.com>
2020-06-13 05:27:09 +0900

    doc: revise baekjoon/15666

--- baekjoon/15666_N과 M(12).cpp
+++ baekjoon/15666_N과 M(12).cpp
@@ -1,41 +1,40 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
-int n, m;
-int arr[8];
-int new_arr[8];
 
-void dfs(int idx, int cnt) {
-	int used[10001] = { false, };
-	
+int n, m;
+vector<int> num;
+vector<int> res;
+
+void dfs(int cnt, int idx) {
+	vector<int> history;
 	if (cnt == m) {
-		for (int i = 0; i < m; i++) {
-			cout << new_arr[i] << " ";
+		for (int i = 0; i < m; i++ ) {
+			cout << res[i] << " ";
 		}
 		cout << "\n";
 		return;
 	}
 
 	for (int i = idx; i < n; i++) {
-		if (!used[arr[i]]) {
-			used[arr[i]] = true;
-			new_arr[cnt++] = arr[i];
-			dfs(i, cnt);
-			cnt--;
-		}
+		if (count(history.begin(), history.end(), num[i]) > 0) continue;
+		history.push_back(num[i]);
+		res[cnt] = num[i];
+		dfs(cnt + 1, i);
 	}
-
 }
 
 int main() {
 	cin >> n >> m;
+	int input;
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> input;
+		num.push_back(input);
+		if (i < m) res.push_back(0);
 	}
+	sort(num.begin(), num.end());
 
-	sort(arr, arr + n);
 	dfs(0, 0);
-
 	return 0;
 }
